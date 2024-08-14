14b6afab03162915525f8b3f90548e67164127df
yuuko-suzuki <31624387+yuuko-suzuki@users.noreply.github.com>
2024-08-14 22:53:28 +0900

    kadai_007アップロード

---
+++ kadai_007/calc.js
@@ -0,0 +1,11 @@
+let num = 30;
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+} else if (num % 5 === 0) {
+  console.log("5の倍数です");
+} else if (num % 3 === 0) {
+  console.log("3の倍数です");
+} else {
+  console.log(num);
+}
